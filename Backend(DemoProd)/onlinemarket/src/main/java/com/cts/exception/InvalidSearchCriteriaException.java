6cd6b8ecd1e3fcc18b64866d5a3be61d06b02f98
Aditya-Singh-S <aditya.singh21c987@cognizant.com>
2025-05-09 10:41:13 +0530

    Delete Backend(DemoProd) directory

--- Backend(DemoProd)/onlinemarket/src/main/java/com/cts/exception/InvalidSearchCriteriaException.java
+++
@@ -1,9 +0,0 @@
-package com.cts.exception;
-
-public class InvalidSearchCriteriaException extends RuntimeException {
-	
-	public InvalidSearchCriteriaException(String message) {
-		super(message);
-	}
-
-}
